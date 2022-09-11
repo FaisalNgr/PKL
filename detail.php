a53e2dfa468861ee8ccaf9027d6ceb0e278eaea7
FaisalNgr <104259690+FaisalNgr@users.noreply.github.com>
2022-09-11 15:06:18 +0700

    Create detail.php

---
+++ detail.php
@@ -0,0 +1,166 @@
+<?php
+// include database connection file
+include_once("koneksi.php");
+
+// Check if form is submitted for user update, then redirect to homepage after update
+$id = $_GET['id'];
+if (isset($_POST['terima'])) {
+    $result = mysqli_query($mysqli, "UPDATE tamu SET status='Diterima' WHERE id=$id");
+
+    header("Location: data.php");
+}
+if (isset($_POST['tolak'])) {
+    $result = mysqli_query($mysqli, "UPDATE tamu SET status='Ditolak' WHERE id=$id");
+
+    header("Location: data.php");
+}
+include('template/cek_login.php');
+?>
+<?php
+// Display selected user data based on id
+// Getting id from url
+$id = $_GET['id'];
+
+// Fetech user data based on id
+$result = mysqli_query($mysqli, "SELECT * FROM tamu WHERE id=$id");
+
+while ($user_data = mysqli_fetch_array($result)) {
+    $nama = $user_data['nama'];
+    $asal_instansi = $user_data['asal_instansi'];
+    $no_telepon = $user_data['no_telepon'];
+    $tanggal = $user_data['tanggal'];
+    $perihal = $user_data['perihal'];
+    $status = $user_data['status'];
+}
+?>
+<html>
+
+<head>
+    <link rel="stylesheet" href="output.css">
+    <link rel="stylesheet" href="faisal.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+</head>
+
+<body class="bg-gray-100 grid grid-ku ">
+    <?php include('template/sidebar.php') ?>
+    <!-- ISI CONTENT -->
+    <div class="p-10 -ml-3" style="background-color:#0d0c22">
+
+
+        <div class="overflow-x-auto pt-6 relative shadow-md sm:rounded-lg">
+            <div class="text-white px-6 rounded mb-3" style="background-color: #12102f;">
+                <div class="flex items-center py-4 overflow-y-auto whitespace-nowrap">
+                    <a href="#" class="text-gray-600 dark:text-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                        </svg>
+                    </a>
+
+                    <span class="mx-5 text-gray-500 dark:text-gray-300 rtl:-scale-x-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+
+                    <a href="data.php" class="text-gray-600 dark:text-gray-200 hover:underline font-bold">
+                        Data
+                    </a>
+
+                    <span class="mx-5 text-gray-500 dark:text-gray-300 rtl:-scale-x-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+
+                    <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline font-bold">
+                        Detail Data
+                    </a>
+                </div>
+            </div>
+            <div class="text-white p-6" style="background-color: #12102f;">
+                <div class="grid grid-cols-2">
+                    <div class="flex flex-col gap-3">
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-red-400 p-2 rounded">
+                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="ml-2">
+                                <p class="text-sm">Nama</p>
+                                <p class="font-semibold"><?= $nama ?></p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-blue-400 p-2 rounded">
+                                <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+                                <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+                                <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+                            </svg>
+
+                            <div class="ml-2">
+                                <p class="text-sm">Asal Instansi</p>
+                                <p class="font-semibold"><?= $asal_instansi ?></p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-green-400 p-2 rounded">
+                                <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="ml-2">
+                                <p class="text-sm">No Telepon</p>
+                                <p class="font-semibold"><?= $no_telepon ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-3">
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-pink-400 p-2 rounded">
+                                <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+                                <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="ml-2">
+                                <p class="text-sm">Tanggal</p>
+                                <p class="font-semibold"><?= $tanggal ?></p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-orange-400 p-2 rounded">
+                                <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                                <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                            </svg>
+
+                            <div class="ml-2">
+                                <p class="text-sm">Perihal</p>
+                                <p class="font-semibold"><?= $perihal ?></p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 bg-purple-400 p-2 rounded">
+                                <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                            </svg>
+
+                            <div class="ml-2">
+                                <p class="text-sm">Status</p>
+                                <p class="font-semibold"><?= $status ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex gap-3 mt-5">
+                    <form method="POST">
+                        <input type="submit" class="bg-green-400 p-3 font-bold rounded text-xl cursor-pointer" value="Terima" name="terima">
+                    </form>
+                    <form method="post">
+                        <input type="submit" class="bg-red-400 px-5 py-3 font-bold rounded text-xl cursor-pointer" value="Tolak" name="tolak">
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
